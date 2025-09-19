4cb2e7eaa4c7342757d17f92dc6d4e39a8b6b008
DhirajKarangale <dakarangale02@gmail.com>
2025-09-20 02:54:19 +0530

    logger updated

--- backend/src/utilities/errorLogger.js
+++ backend/src/utilities/errorLogger.js
@@ -4,11 +4,11 @@ const path = require('path');
 function ErrorLogger(error, req, res, next) {
     if (error) {
         const data = new Date().toString() + " " + req.method + " " + req.url + "->" + error.stack + "\n\n";
-        const filePath = path.join(__dirname, '../logs/ErrorLogger.txt');
         console.log("Error: " + data);
-        fs.appendFile(filePath, data, (err) => {
-            if (err) console.log("Failed to add error log");
-        });
+        // const filePath = path.join(__dirname, '../logs/ErrorLogger.txt');
+        // fs.appendFile(filePath, data, (err) => {
+        //     if (err) console.log("Failed to add error log");
+        // });
 
         if (!error.status) error.status = 500;
         res.status(error.status).json(error.message);
