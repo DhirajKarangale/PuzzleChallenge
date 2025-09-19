edf10252942e2d5e525971730527d65ca5635d7a
DhirajKarangale <dakarangale02@gmail.com>
2025-09-20 02:45:42 +0530

    Router bug fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,7 +21,7 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
-app.use('/webhook', webhookRouter);
+app.use('/webhook', webhook);
 
 (async () => {
     try {
